0e1b8ddc11d081335c5ecb7a7b4e5c15187a322e
dakota002 <dakota.d@live.com>
2023-06-08 10:39:11 -0400

    removes extra quotes

--- update-tags.mjs
+++ update-tags.mjs
@@ -9,4 +9,4 @@ const mssg = 'Update version ' + tag
 const newBranchName = `Update-${tag}`
 console.log(`Checking out branch:${newBranchName} and pushing`)
 //execSync(`echo \"${mssg}\"`, { stdio: [0, 1, 2] })
-execSync(`git checkout -b ${newBranchName} && git add . && git commit -m \"'${mssg}'\" && git push --set-upstream origin ${newBranchName}`, { stdio: [0, 1, 2] });
+execSync(`git checkout -b ${newBranchName} && git add . && git commit -m \"${mssg}\" && git push --set-upstream origin ${newBranchName}`, { stdio: [0, 1, 2] });
