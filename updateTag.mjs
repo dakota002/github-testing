f16cb25d046726df63856947bd3fb43b5053f090
dakota002 <dakota.d@live.com>
2023-06-14 10:14:21 -0400

    Check for reset flag

--- updateTag.mjs
+++ updateTag.mjs
@@ -1,7 +1,13 @@
 import { readFile, writeFile } from 'fs/promises'
 
 
+async function main() {
+    if (process.argv.some(x => x == '--reset')) {
+        console.log('Reset paths to main')
+    } else {
+        const version = JSON.parse(await readFile('package.json')).version
+        console.log(`Updating to version:${version}`)
+    }
+}
 
-const version = JSON.parse(await readFile('package.json')).version
-
-console.log(`Updating to version:${version}`)
+await main()
