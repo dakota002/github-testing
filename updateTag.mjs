a08cc33f73a2facdd3b5d5644465909c6b1224f9
dakota002 <dakota.d@live.com>
2023-06-22 11:55:53 -0400

    -m

--- updateTag.mjs
+++ updateTag.mjs
@@ -12,7 +12,7 @@ async function main() {
         await writeFile('misc/testJunk.txt', `I have been updated to version:${version}`)
         execSync('git config user.name "dakota002"')
         execSync('git config user.email "dakota002@users.noreply.github.com"')
-        execSync('git add . && git commit - m \"Update version\" && git push', { stdio: [0, 1, 2] });
+        execSync('git add . && git commit -m "Update version" && git push', { stdio: [0, 1, 2] });
         console.log(version)
     }
 }
