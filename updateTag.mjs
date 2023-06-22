080a867d32112a4fba8e50b8d3ea293970162699
dakota002 <dakota.d@live.com>
2023-06-22 10:54:46 -0400

    remove logs

--- updateTag.mjs
+++ updateTag.mjs
@@ -4,10 +4,10 @@ import { readFile, writeFile } from 'fs/promises'
 async function main() {
     if (process.argv.some(x => x == '--reset')) {
         await writeFile('misc/testJunk.txt', 'I have been reset')
-        console.log('Reset paths to main')
+        //console.log('Reset paths to main')
     } else {
         const version = JSON.parse(await readFile('package.json')).version
-        console.log(`Updating to version:${version}`)
+        //console.log(`Updating to version:${version}`)
         await writeFile('misc/testJunk.txt', `I have been updated to version:${version}`)
     }
 }
