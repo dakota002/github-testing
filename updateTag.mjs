6d1d461a0817dff1b4167569128a2e29f930815a
dakota002 <dakota.d@live.com>
2023-06-22 11:44:16 -0400

    pain

--- updateTag.mjs
+++ updateTag.mjs
@@ -10,7 +10,9 @@ async function main() {
         const version = JSON.parse(await readFile('package.json')).version
         //console.log(`Updating to version:${version}`)
         await writeFile('misc/testJunk.txt', `I have been updated to version:${version}`)
-        execSync('git config user.name "${{github.actor}}" && git config user.email "${{github.actor}}@users.noreply.github.com" && git add . && git commit - m \"Update version\" && git push', { stdio: [0, 1, 2] });
+        execSync('git config user.name "${{github.actor}}"')
+        execSync('git config user.email "${{github.actor}}@users.noreply.github.com"')
+        execSync('git add . && git commit - m \"Update version\" && git push', { stdio: [0, 1, 2] });
         console.log(version)
     }
 }
