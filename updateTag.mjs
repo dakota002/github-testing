ef40091075dc3509cb677e4355ba9d972c90ccf4
dakota002 <dakota.d@live.com>
2023-06-22 11:24:43 -0400

    Adds back commands

--- updateTag.mjs
+++ updateTag.mjs
@@ -9,6 +9,7 @@ async function main() {
         const version = JSON.parse(await readFile('package.json')).version
         //console.log(`Updating to version:${version}`)
         await writeFile('misc/testJunk.txt', `I have been updated to version:${version}`)
+        execSync(`git add . && git commit -m \"Update version\" && git push`, { stdio: [0, 1, 2] });
         console.log(version)
     }
 }
