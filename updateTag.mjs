fcf6eacc04b9350a35a88acf906342632657292a
dakota002 <dakota.d@live.com>
2023-06-22 15:45:26 -0400

    Expanding on the version command

--- updateTag.mjs
+++ updateTag.mjs
@@ -10,9 +10,7 @@ async function main() {
         const version = JSON.parse(await readFile('package.json')).version
         //console.log(`Updating to version:${version}`)
         await writeFile('misc/testJunk.txt', `I have been updated to version:${version}`)
-        execSync('git config user.name "dakota002"')
-        execSync('git config user.email "dakota002@users.noreply.github.com"')
-        execSync('git add . && git commit -m "Update version" && git push', { stdio: [0, 1, 2] });
+        // execSync('git add . && git commit -m "Update version" && git push', { stdio: [0, 1, 2] });
         console.log(version)
     }
 }
