2c31b77dd5a50988b5764bdf093b3da9a233c858
dakota002 <dakota.d@live.com>
2023-07-05 09:28:46 -0400

    testing import

--- updateTag.mjs
+++ updateTag.mjs
@@ -1,12 +1,12 @@
 import { readFile, writeFile } from 'fs/promises'
-
+import { version } from './package.json';
 
 async function main() {
     if (process.argv.some(x => x == '--reset')) {
         await writeFile('misc/testJunk.txt', 'I have been reset')
         //console.log('Reset paths to main')
     } else {
-        const version = JSON.parse(await readFile('package.json')).version
+        //const version = JSON.parse(await readFile('package.json')).version
         //console.log(`Updating to version:${version}`)
         await writeFile('misc/testJunk.txt', `I have been updated to version:${version}`)
         console.log(version)
